d37aa59b5b45bfb1151adb5452b226af5f24c1da
Robert Wang <rwang5688@outlook.com>
2022-07-14 23:34:41 -0700

    change to multibert_classifier

--- sm-model/multibert.py
+++ sm-model/multibert.py
@@ -57,11 +57,10 @@ if __name__ == "__main__":
     train_data, train_labels = _load_training_data(args.train)
     eval_data, eval_labels = _load_testing_data(args.train)
 
-    mnist_classifier = model(train_data, train_labels, eval_data, eval_labels)
+    multibert_classifier = model(train_data, train_labels, eval_data, eval_labels)
 
     if args.current_host == args.hosts[0]:
         # save model to an S3 directory with version number '00000001' in Tensorflow SavedModel Format
         # To export the model as h5 format use model.save('my_model.h5')
-        mnist_classifier.save(os.path.join(args.sm_model_dir, '000000001'))
+        multibert_classifier.save(os.path.join(args.sm_model_dir, '000000001'))
 
-        
